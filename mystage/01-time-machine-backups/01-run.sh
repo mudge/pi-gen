41a90fac1cd533730aaa3af70cfd3d084fa68ab4
Paul Mucur <mudge@mudge.name>
2020-06-14 14:34:37 +0100

    Only set up Time Machine if external drive is set

--- mystage/01-time-machine-backups/01-run.sh
+++ mystage/01-time-machine-backups/01-run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
 # Set up Time Machine backups
-echo "Installing Samba config and Avahi service"
-cat << EOF >> "${ROOTFS_DIR}/etc/samba/smb.conf"
+if [ -v EXTERNAL_DRIVE_UUID ]; then
+  echo "Installing Samba config and Avahi service"
+  cat << EOF >> "${ROOTFS_DIR}/etc/samba/smb.conf"
 [backups]
    comment = Backups
    path = /media/backups
@@ -11,19 +12,22 @@ cat << EOF >> "${ROOTFS_DIR}/etc/samba/smb.conf"
    vfs objects = catia fruit streams_xattr
    fruit:time machine = yes
 EOF
-install -m 644 files/samba.service "${ROOTFS_DIR}/etc/avahi/services"
+  install -m 644 files/samba.service "${ROOTFS_DIR}/etc/avahi/services"
 
-echo "Adding external drive to /etc/fstab"
-echo "UUID=${EXTERNAL_DRIVE_UUID}       /media  ext4    sync,noexec,nodev,noatime,nodiratime    0       0" >> "${ROOTFS_DIR}/etc/fstab"
+  echo "Adding external drive to /etc/fstab"
+  echo "UUID=${EXTERNAL_DRIVE_UUID}       /media  ext4    sync,noexec,nodev,noatime,nodiratime    0       0" >> "${ROOTFS_DIR}/etc/fstab"
 
-echo "Configuring external drive spindown time"
-cat << EOF >> "${ROOTFS_DIR}/etc/hdparm.conf"
+  echo "Configuring external drive spindown time"
+  cat << EOF >> "${ROOTFS_DIR}/etc/hdparm.conf"
 /dev/disk/by-uuid/${EXTERNAL_DRIVE_UUID} {
-	spindown_time = 120
+        spindown_time = 120
 }
 EOF
 
-echo "Adding ${FIRST_USER_NAME} as Samba user"
-on_chroot << EOF
+  echo "Adding ${FIRST_USER_NAME} as Samba user"
+  on_chroot << EOF
 (echo "${FIRST_USER_PASS}"; echo "${FIRST_USER_PASS}") | smbpasswd -s -a "${FIRST_USER_NAME}"
 EOF
+else
+  echo "No EXTERNAL_DRIVE_UUID set, skipping Time Machine setup"
+fi
